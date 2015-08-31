037051ddf89ce6eb497cbd7ef62167720041b08f
asteph <alissa.stephens1@gmail.com>
2015-08-31 15:34:09 -0500

    changed href for simple simon so that it works with domain name

--- app/views/portfolio.blade.php
+++ app/views/portfolio.blade.php
@@ -11,5 +11,5 @@
 <p style="text-indent: 50px">
 	Simple Simon is a game with 4 colors that play randomly and continue to add one color to the sequence at each turn. You must then select the correct order back to the game for as many rounds as you can. Created using jQuery.
 </p>
-<iframe style="width: 80%; height: 600px; margin: 20px 10% 20px 10%" src="http://blog.dev/simplesimon"></iframe>
+<iframe style="width: 80%; height: 600px; margin: 20px 10% 20px 10%" src="/simplesimon"></iframe>
 @stop
